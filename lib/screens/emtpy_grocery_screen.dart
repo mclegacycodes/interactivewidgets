cd7f1d6c1e90c21ca760b57e84a1b992597243c9
unknown <kingsleytwum2@gmail.com>
2022-07-13 18:24:04 +0000

    first commit

---
+++ lib/screens/emtpy_grocery_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class EmptyGroceryScreen extends StatelessWidget {
+  const EmptyGroceryScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+   return Padding(
+ padding: const EdgeInsets.all(30.0),
+ // 2
+ child: Center(
+ // 3
+ child: Column(
+ mainAxisAlignment: MainAxisAlignment.center,
+ children: [
+Flexible(child: AspectRatio(aspectRatio: 1/1, 
+child: Image.asset('assets/fooderlich_assets/empty_list.png'),)),
+ // TODO 5: Add empty screen title
+ // TODO 6: Add empty screen subtitle
+ // TODO 7: Add browse recipes button
+ ],
+ ),
+ ),
+);
+
+  }
+}
---
+++ lib/screens/grocery_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/screens/emtpy_grocery_screen.dart';
+
+class GroceryScreen extends StatelessWidget {
+  const GroceryScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //todo:add a scaffold widget
+    return EmptyGroceryScreen();
+    
+  }
+
+  //todo: add buildgroceryscreen
+}
